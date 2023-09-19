03843aa190f00731a4bd515d84116c33f31c8de0
Andrea Wen-Yi Wang <ayw255@nyu.edu>
2023-09-19 16:53:59 -0400

    add argument MALLET to specify mallet path

--- get_input.sh
+++ get_input.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 
-IN_SEQ=$1
-OUT_TSV=$2
-MIN_DOC_LEN=${3:-0}
+MALLET=$1
+IN_SEQ=$2
+OUT_TSV=$3
+MIN_DOC_LEN=${4:-0}
 
 
 MIN_TOKENS=$(( MIN_DOC_LEN + 2 ))
 
 echo 'Generating intermediate tsv file'
-mallet info --input $IN_SEQ --print-matrix siw > $OUT_TSV
+$MALLET info --input $IN_SEQ --print-matrix siw > $OUT_TSV
 sed -i -e '/^$/d' -e 's/ /\t/' -e 's/ /\t/' $OUT_TSV # Convert to tsv
 
 if [ $MIN_DOC_LEN -ne 0 ]; then 
--- get_vocab.sh
+++ get_vocab.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
+MALLET=$1
+IN_SEQ=$2
+VOCAB=$3
 
-IN_SEQ=$1
-VOCAB=$2
-
-mallet info --input $IN_SEQ --print-feature-counts | cut -f 1 | sort -k 1 > $VOCAB
+$MALLET info --input $IN_SEQ --print-feature-counts | cut -f 1 | sort -k 1 > $VOCAB
